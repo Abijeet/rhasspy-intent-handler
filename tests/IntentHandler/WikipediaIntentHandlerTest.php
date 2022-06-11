ffeb8264492e22a34bc320be92e0e727913b465b
Abijeet <abijeetpatro@gmail.com>
2022-06-11 21:55:48 +0530

    Fix failing tests for WikipediaIntentHandler

--- tests/IntentHandler/WikipediaIntentHandlerTest.php
+++ tests/IntentHandler/WikipediaIntentHandlerTest.php
@@ -3,19 +3,42 @@ declare(strict_types=1);
 
 namespace Tests\IntentHandler;
 
+use App\IntentHandlers\WikipediaIntentHandler;
+use App\Models\Intent;
 use App\SearchQuery\Builders\QueryBuilder;
+use App\SearchQuery\Handlers\WikipediaQueryHandler;
 use Tests\TestCase;
 
 class WikipediaIntentHandlerTest extends TestCase
 {
 	public function testQueryResultNotFound()
 	{
+		$queryString = 'search string';
 		$queryBuilder = $this->getMockBuilder(QueryBuilder::class)
 			->disableOriginalConstructor()
 			->getMock();
 
 		$queryBuilder->expects($this->once())
 			->method('get')
-			->will($this->returnValue('Invalid string 1234545'));
+			->will($this->returnValue($queryString));
+
+		$queryHandler = $this->getMockBuilder(WikipediaQueryHandler::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		$queryHandler->expects($this->once())
+			->method('getQueryResult')
+			->with($queryString);
+
+		$intentHandler = new WikipediaIntentHandler($queryBuilder, $queryHandler);
+		$intentHandler->handle(
+			new Intent(
+				'wikipedia',
+				0.6,
+				[],
+				'hello',
+				'hello'
+			)
+		);
 	}
 }
