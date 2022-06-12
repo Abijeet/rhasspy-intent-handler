43a00f43b577ccf0d4898494356b9ef32a83be7f
Abijeet <abijeetpatro@gmail.com>
2022-06-12 10:14:08 +0530

    Add tests for WikipediaIntentHandler

--- tests/IntentHandler/WikipediaIntentHandlerTest.php
+++ tests/IntentHandler/WikipediaIntentHandlerTest.php
@@ -3,35 +3,30 @@ declare(strict_types=1);
 
 namespace Tests\IntentHandler;
 
+use App\Exceptions\QueryResultNotFound;
 use App\IntentHandlers\WikipediaIntentHandler;
 use App\Models\Intent;
 use App\SearchQuery\Builders\QueryBuilder;
 use App\SearchQuery\Handlers\WikipediaQueryHandler;
+use App\SearchQuery\Results\WikipediaQueryResult;
 use Tests\TestCase;
 
 class WikipediaIntentHandlerTest extends TestCase
 {
-	public function testQueryResultNotFound()
+	public function testEmptyQuery(): void
 	{
-		$queryString = 'search string';
-		$queryBuilder = $this->getMockBuilder(QueryBuilder::class)
-			->disableOriginalConstructor()
-			->getMock();
-
-		$queryBuilder->expects($this->once())
-			->method('get')
-			->will($this->returnValue($queryString));
+		$queryString = '';
+		$queryBuilder = $this->getQueryBuilder($queryString);
 
 		$queryHandler = $this->getMockBuilder(WikipediaQueryHandler::class)
 			->disableOriginalConstructor()
 			->getMock();
 
-		$queryHandler->expects($this->once())
-			->method('getQueryResult')
-			->with($queryString);
+		$queryHandler->expects($this->never())
+			->method('getQueryResult');
 
 		$intentHandler = new WikipediaIntentHandler($queryBuilder, $queryHandler);
-		$intentHandler->handle(
+		$result = $intentHandler->handle(
 			new Intent(
 				'wikipedia',
 				0.6,
@@ -40,5 +35,81 @@ class WikipediaIntentHandlerTest extends TestCase
 				'hello'
 			)
 		);
+
+		$this->assertEquals(__('empty_search_query'), $result);
+	}
+
+	public function testQuery(): void
+	{
+		$queryString = 'new delhi';
+		$queryResultString = 'extracted text';
+		$queryBuilder = $this->getQueryBuilder($queryString);
+
+		$result = $this->getMockBuilder(WikipediaQueryResult::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$result->expects($this->once())
+			->method('getResult')
+			->willReturn($queryResultString);
+
+		$queryHandler = $this->getMockBuilder(WikipediaQueryHandler::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$queryHandler->expects($this->once())
+			->method('getQueryResult')
+			->willReturn($result);
+
+
+		$intentHandler = new WikipediaIntentHandler($queryBuilder, $queryHandler);
+		$result = $intentHandler->handle(
+			new Intent(
+				'wikipedia',
+				0.5,
+				[],
+				'hello',
+				'hello'
+			)
+		);
+
+		$this->assertEquals($queryResultString, $result);
+	}
+
+	public function testQueryResultNotFound(): void
+	{
+		$queryString = 'new delhi';
+		$queryBuilder = $this->getQueryBuilder($queryString);
+
+		$queryHandler = $this->getMockBuilder(WikipediaQueryHandler::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$queryHandler->expects($this->once())
+			->method('getQueryResult')
+			->willThrowException(new QueryResultNotFound('not found'));
+
+		$intentHandler = new WikipediaIntentHandler($queryBuilder, $queryHandler);
+		$result = $intentHandler->handle(
+			new Intent(
+				'wikipedia',
+				0.5,
+				[],
+				'hello',
+				'hello'
+			)
+		);
+
+		$this->assertEquals(__('rhasspy_wiki_query_not_found', ['query' => $queryString]), $result);
+	}
+
+	private function getQueryBuilder(string $query)
+	{
+		$queryBuilder = $this->getMockBuilder(QueryBuilder::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		$queryBuilder->expects($this->once())
+			->method('get')
+			->will($this->returnValue($query));
+
+		return $queryBuilder;
 	}
 }
